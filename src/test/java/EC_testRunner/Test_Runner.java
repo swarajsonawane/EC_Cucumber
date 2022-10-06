0fabd1ea5ae036f2b71f368cb0fc6dfbfc6270eb
swarajsonawane <swarajsonawane25@gmail.com>
2022-10-06 10:38:52 +0530

    Commited

---
+++ src/test/java/EC_testRunner/Test_Runner.java
@@ -0,0 +1,19 @@
+package EC_testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Swaraj Sonawane\\eclipse-workspace\\Ecom_Cucumber_Framework\\src\\test\\java\\FeatureFiles\\Products.feature",
+glue= {"EC_StepDeifination"},
+monochrome = true,
+dryRun = false,
+plugin = {"pretty","html:target/HTML/Result.html",   //generating pretty folder for report 
+		"junit:target/XML/Result.xml",
+		"json:target/jsonReports/cucumber-report.json"
+/*"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  --->compile---> test---> verify	*/})
+public class Test_Runner {
+
+}
